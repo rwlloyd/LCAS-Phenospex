7fd6c759b01974e467b4200e020a0c38620f0f8b
Rob Lloyd <r.w.lloyd@gmail.com>
2021-04-22 17:33:54 +0100

    A bit more work. Needs the socket sorting.

--- public/sketch.js
+++ public/sketch.js
@@ -1,72 +1,68 @@
 // P5.js sketch served as the interface for LCAS-Phenospex 
 
-// Based on
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
-
 // Keep track of our socket connection
 var socket;
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
-
-  // DOM element for the desired block-id
-  const blockId = null;
-  let blockIDinput = createInput('');
-  blockIDinput.position(50,10);
-  blockIDinput.size(100);
-  blockIDinput.input(blockIdCallback);
-
-
-
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
   socket = io.connect('http://localhost:3000');
   //socket = io.connect('http://192.168.1.84:3000')
   // We make a named event called 'newData' and write an
   // anonymous callback function
-  socket.on('newData',
-    // When we receive data
-    function(data) {
-      console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill(0,0,255);
-      noStroke();
-      ellipse(data.x, data.y, 20, 20);
-    }
+  // socket.on('newData',
+  //   // When we receive data
+  //   function (data) {
+  //     console.log("Got: " + data.x + " " + data.y);
+  //     // Draw a blue circle
+  //     fill(0, 0, 255);
+  //     noStroke();
+  //     ellipse(data.x, data.y, 20, 20);
+  //   }
+  // );
+
+  //createCanvas(400, 400);
+  // background(0);
+  noCanvas();
+  
+  // DOM element for the desired block-id
+  let blockId = null;
+  let input = createInput('');
+  input.position(50, 10);
+  input.size(100);
+  input.input(blockIdCallback);
+
+  let button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(sendBlockId(blockId));
+
+  let doclink = createSpan(
+    // <a href="RESTAPI-doc.html">Planteye RESTAPI Documentation</a>
+    "Planteye RESTAPI Documentation"
   );
+  doclink.position(20, windowHeight - 20);
+}
+
+function blockIdCallback() {
+  console.log('you are typing...', this.value());
+  blockId = this.value();
+  sendBlockId(blockId)
+}
+
+// Function for sending to the socket
+function sendBlockId(blockId) {
+  // We are sending!
+  console.log("blockID to send: " + blockId);
+
+  // Make a little object with  and y
+  var data = {
+    ID: blockId
+  };
+
+  // Send that object to the socket
+  socket.emit('newData', data);
 }
 
 function draw() {
   // Nothing
-}
-
-function blockIdCallback(){
-  console.log('you are typing...', 
-  this.value());
-}
-
-function mouseDragged() {
-  // Draw some white circles
-  fill(255);
-  noStroke();
-  ellipse(mouseX,mouseY,20,20);
-  // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
-}
-
-// Function for sending to the socket
-function sendmouse(xpos, ypos) {
-  // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
-  
-  // Make a little object with  and y
-  var newData = {
-    x: xpos,
-    y: ypos
-  };
-
-  // Send that object to the socket
-  socket.emit('newData', newData);
 }
